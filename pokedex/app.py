9c15172bed78a3766527d3b635d67779f58a3ced
Ritwick Dsouza <hello@ritwickdsouza.com>
2020-08-22 17:38:19 +0400

    Create pokedex flask app

---
+++ pokedex/app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+
+from pokedex.search import search
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    all_pokemons = search()
+    return render_template(
+        'index.html',
+        pokemons=all_pokemons
+    )
+
+
+@app.route('/result')
+def search_pokemon():
+    query = request.args.get('search_query')
+    pokemons_for_query = search(query)
+    return render_template(
+        'index.html',
+        pokemons=pokemons_for_query
+    )
